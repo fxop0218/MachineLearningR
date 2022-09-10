b731e911cc55fbde64f4ff4528bcf48e7bd995b4
fxop0218 <oliverasfrancescins@gmail.com>
2022-09-10 13:37:40 +0200

    RFC added and saved

--- RandomForestClass.R
+++ RandomForestClass.R
@@ -52,7 +52,7 @@ grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('Age', 'EstimatedSalary')
 y_grid = predict(classifier, newdata = grid_set, type = "class")
 plot(set[, -3],
-     main = 'Random forest  (Conjunto de Entrenamiento)',
+     main = 'Random foresst  (Conjunto de Entrenamiento)',
      xlab = 'Edad', ylab = 'Sueldo Estimado',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
